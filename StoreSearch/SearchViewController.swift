54a1030371541db039ef51cb8e8b5811dd03b9e7
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-19 00:06:04 -0500

    register nib file for use in code

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -14,9 +14,12 @@ class SearchViewController: UIViewController {
     var searchResults = [SearchResult]()
     var hasSearched = false
 
+    //MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 51, left: 0, bottom: 0, right:  0)
+        let cellNib = UINib(nibName: "SearchResultCell", bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: "SearchResultCell")
     }
 
 
@@ -55,20 +58,16 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "SearchResultCell"
         
-        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-        if cell == nil {
-            cell = UITableViewCell(
-                style: .subtitle, reuseIdentifier: cellIdentifier)
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         if searchResults.count == 0 {
-            cell?.textLabel!.text = "(Nothing found)"
-            cell?.detailTextLabel!.text = ""
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
         } else {
         let searchResult = searchResults[indexPath.row]
-            cell?.textLabel!.text = searchResult.name
-            cell?.detailTextLabel!.text = searchResult.artistName
+            cell.textLabel!.text = searchResult.name
+            cell.detailTextLabel!.text = searchResult.artistName
         }
-        return cell!
+        return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
