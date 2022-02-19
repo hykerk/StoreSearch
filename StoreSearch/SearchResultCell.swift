59de4fc9b837e84dc56f319ed2b3d75201b9b710
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-19 00:15:38 -0500

    Add cocoa touch class for SearchResultCell and create and connect outlets for the labels and image view

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Katellyn Hyker on 2/19/22.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    @IBOutlet weak var nameLabl: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
