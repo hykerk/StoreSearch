20b5a514f912886a411293306f4fb4607fc6f5a2
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-22 18:06:08 -0500

    end chapter 33

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+
     
     var searchResults = [SearchResult]()
     var hasSearched = false
