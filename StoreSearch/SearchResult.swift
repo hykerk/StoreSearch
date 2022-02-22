2df6da88145a57c7029ddcb309167a7207e7581e
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-22 18:23:04 -0500

    prepare to parse JSON data

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -7,7 +7,16 @@
 
 import Foundation
 
-class SearchResult {
-    var name = ""
-    var artistName = ""
+class ResultArray: Codable {
+    var resultCount = 0
+    var results = [SearchResult]()
+    
+}
+
+class SearchResult: Codable {
+    var artistName: String? = ""
+    var trackName: String? = ""
+    var name: String {
+        return trackName ?? ""
+    }
 }
