af4796e2cdf1015bf7108b63c75c7e81735f4c67
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-23 12:30:24 -0500

    UIIMageView extension for downloading images

---
+++ StoreSearch/UIImageView+DownloadImage.swift
@@ -0,0 +1,29 @@
+//
+//  UIImageView+DownloadImage.swift
+//  StoreSearch
+//
+//  Created by Katellyn Hyker on 2/23/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func loadImage(url: URL) -> URLSessionDownloadTask {
+        let session = URLSession.shared
+        let downloadTask = session.downloadTask(with: url) {
+            [weak self] url, _, error in
+            if error == nil, let url = url,
+               let data = try? Data(contentsOf: url),
+               let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    if let weakSelf = self {
+                        weakSelf.image = image
+                    }
+                }
+            }
+        }
+        downloadTask.resume()
+        return downloadTask
+    }
+}
