3cd6884e1c08acf59110906d06b772da57b5fbdc
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-22 19:38:26 -0500

    extreme synchronous networking

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -35,7 +35,7 @@ class SearchViewController: UIViewController {
     //MARK: - Helper methods
     func iTunesURL(searchText: String) -> URL {
         let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
-        let urlString = String(format: "https://itunes.apple.com/search?term=%@", encodedText)
+        let urlString = String(format: "https://itunes.apple.com/search?term=%@&limit=200", encodedText)
         let url = URL(string: urlString)
         return url!
     }
