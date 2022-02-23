637c98c21cd80b84a2173a5b11c5eb64c98d71e6
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-22 19:20:16 -0500

    differing  data structures

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -17,7 +17,7 @@ class SearchResult: Codable, CustomStringConvertible {
     var artistName: String? = ""
     var trackName: String? = ""
     var name: String {
-        return trackName ?? ""
+        return trackName ?? collectionName ?? ""
     }
     var description: String {
         return "\nResult - Kind:   \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
@@ -27,15 +27,36 @@ class SearchResult: Codable, CustomStringConvertible {
     var currency = ""
     var imageSmall = ""
     var imageLarge = ""
-    var storeURL: String? = ""
-    var  genre = ""
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
+    var storeURL: String {
+        return trackViewUrl ?? collectionViewUrl ??  ""
+    }
+    var price: Double {
+        return trackPrice ?? itemPrice ?? 0.0
+    }
+    var genre: String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+        return ""
+    }
     
     enum CodingKeys: String, CodingKey {
         case imageSmall = "artworkUrl60"
         case imageLarge = "artworkUrl100"
-        case storeURL = "trackViewUrl"
-        case genre = "primaryGenreName"
-        case kind, artistName, trackName
-        case trackPrice, currency
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
     }
 }
