57d9d4f2e9018d506704cd165f7c4553a86a33f9
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-22 19:29:47 -0500

    sort search results

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -78,6 +78,7 @@ extension SearchViewController: UISearchBarDelegate {
             print("URL: '\(url)'")
             if let data = performStoreRequest(with: url) {
                 searchResults = parse(data: data)
+                searchResults.sort  { result1, result2 in return result1.name.localizedStandardCompare(result2.name) == .orderedAscending }
             }
             tableView.reloadData()
             }
