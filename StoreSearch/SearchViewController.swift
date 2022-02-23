69aeca512efd88d91395c3da3a230aaefcbd8d12
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-23 11:49:24 -0500

    cancel operations

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -16,6 +16,8 @@ class SearchViewController: UIViewController {
     var hasSearched = false
     
     var isLoading = false
+    
+    var dataTask: URLSessionDataTask?
 
     //MARK: - viewDidLoad
     override func viewDidLoad() {
@@ -66,7 +68,7 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if !searchBar.text!.isEmpty {
             searchBar.resignFirstResponder()
-            
+            dataTask?.cancel()
             isLoading = true
             tableView.reloadData()
             
@@ -75,9 +77,9 @@ extension SearchViewController: UISearchBarDelegate {
             
             let url = iTunesURL(searchText: searchBar.text!)
             let session = URLSession.shared
-            let dataTask = session.dataTask(with: url) {data, response, error in
-                if let error = error {
-                    print("Failure! \(error.localizedDescription)")
+            dataTask = session.dataTask(with: url) {data, response, error in
+                if let error = error as NSError?, error.code == -999 {
+                    return
                 } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
                     if let data = data {
                         self.searchResults = self.parse(data: data)
@@ -98,7 +100,7 @@ extension SearchViewController: UISearchBarDelegate {
                     self.showNetworkError()
                 }
             }
-            dataTask.resume()
+            dataTask?.resume()
         }
     }
     func position(for bar: UIBarPositioning) -> UIBarPosition {
