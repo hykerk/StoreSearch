833711be61f7f981dc0425d9de5e40780d342982
hykerk <97618991+hykerk@users.noreply.github.com>
2022-02-23 12:25:58 -0500

    search result cell refactoring

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -22,5 +22,13 @@ class SearchResultCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-
+//MARK: - Helper Methods
+    func configure(for result: SearchResult) {
+        nameLabel.text = result.name
+        if result.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = String(format: "%@ (%@)", result.artist, result.type)
+        }
+    }
 }
